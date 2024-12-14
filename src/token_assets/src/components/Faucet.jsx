71042353bec3baf1d634460e273f53f2aa928430
Kunal Bhatnagar <72562791+KunalBhatnagar@users.noreply.github.com>
2024-12-14 17:17:25 -0500

    Update Faucet.jsx

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -31,7 +31,7 @@ function Faucet(props) {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 10,000 Koin tokens to {props.userPrincipal}.</label>
+      <label>Get your free Koin tokens here! Claim 10,000 Koin tokens to {props.userPrincipal}.</label>
       <p className="trade-buttons">
         <button id="btn-payout" onClick={handleClick} disabled = {disable}>
           {buttonText}
